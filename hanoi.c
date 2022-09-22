104ec1bb3f4186b7ca18916986c3971ec6653d22
kyska11 <100410828+kyska11@users.noreply.github.com>
2022-09-22 10:20:22 +0200

    Update hanoi.c
    Oprava chyb v texte

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("prens kamen 1 z %d na %d\n", a, b);
+    printf("prenes kamen 1 z %d na %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("prens kamen %d z %d na %d\n", n, a, b);
+    printf("prenes kamen %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
